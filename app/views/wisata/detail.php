bd0ccced203c0ada54fa245463918ea8b5504c64
budionoturman <budionoturman@gmail.com>
2022-01-22 17:12:54 +0700

    menghapus gambar pada views/detail.php

--- app/views/wisata/detail.php
+++ app/views/wisata/detail.php
@@ -1,6 +1,5 @@
 <div class="container mt-5">
     <div class="card" style="width: 18rem;">
-    <img src="../public/img/wallpaperbetter.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title"><?= $data['wst']['nama']; ?></h5>
             <p class="card-text mt-4"><?= $data['wst']['lokasi']; ?></p>
